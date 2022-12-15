887c54bee265560ddd098c3df0c03414882a928a
PStewart1 <4920@holbertonstudents.com>
2022-12-15 00:41:42 -0600

    added scrolling camera, and player/tile overlap

--- code/level.py
+++ code/level.py
@@ -7,12 +7,12 @@ from debug import debug
 
 class Level:
     def __init__(self):
-
+        # get the display surface
         self.display_surface = pygame.display.get_surface()
-        
-        self.visible_sprites = pygame.sprite.Group()
+        # sprite group setup
+        self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
-
+        # sprite setup
         self.create_map()
 
     def create_map(self):
@@ -26,6 +26,25 @@ class Level:
                     self.player = Player((x,y), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
-        self.visible_sprites.draw(self.display_surface)
+        # update and draw the game
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.direction)
+
+
+class YSortCameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        # general setup
+        super().__init__()
+        self.display_surface = pygame.display.get_surface()
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] // 2
+        self.offset = pygame.math.Vector2()
+
+    def custom_draw(self, player):
+        # getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+
+        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
+            self.display_surface.blit(sprite.image, offset_pos)
