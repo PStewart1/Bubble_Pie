0aa949b38b3a6ce3acd861b1cae38bc8a340515a
PStewart1 <4920@holbertonstudents.com>
2022-12-15 07:43:21 -0600

    added UI

--- code/level.py
+++ code/level.py
@@ -6,6 +6,8 @@ from debug import debug
 from support import *
 from random import choice
 from weapon import Weapon
+from ui import UI
+
 
 class Level:
     def __init__(self):
@@ -21,6 +23,9 @@ class Level:
 
         # sprite setup
         self.create_map()
+
+        # user interface
+        self.ui = UI()
 
     def create_map(self):
         layouts = {
@@ -63,7 +68,7 @@ class Level:
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
-        debug(self.player.status)
+        self.ui.display(self.player)
 
 
 class YSortCameraGroup(pygame.sprite.Group):
